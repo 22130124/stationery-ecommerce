187c88b0ea1ec5b7f81deff2db16479d269f40e5
Truong Quy Khoa <22130124@st.hcmuaf.edu.vn>
2025-10-10 19:12:19 +0700

    chore: Set up initial project scaffolding and dependencies

---
+++ stationery-ecommerce-backend/src/main/java/com/tqk/stationeryecommercebackend/StationeryEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tqk.stationeryecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationeryEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StationeryEcommerceBackendApplication.class, args);
+    }
+
+}
