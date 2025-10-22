d05d7d70fc244b1a628292afff010d3ff4548ee5
Truong Quy Khoa <22130124@st.hcmuaf.edu.vn>
2025-10-22 19:03:31 +0700

    feat(shopping-cart): store cart items temporarily in localStorage when adding product

--- stationery-ecommerce-frontend/src/App.js
+++ stationery-ecommerce-frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import AppRoutes from "./routes/AppRoutes";
 import {BrowserRouter} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import AppToaster from "./components/AppToaster";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <AppRoutes/>
+                <AppToaster/>
             </div>
         </BrowserRouter>
     );
---
+++ stationery-ecommerce-frontend/src/components/AppToaster.jsx
@@ -0,0 +1,38 @@
+// src/components/common/AppToaster.jsx
+
+import { Toaster } from 'react-hot-toast';
+
+const toastOptions = {
+    position: "top-right",
+    style: {
+        background: '#363636',
+        color: '#fff',
+        borderRadius: '10px',
+        boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+        fontSize: '15px',
+        padding: '12px 18px',
+    },
+
+    success: {
+        duration: 2500,
+        iconTheme: {
+            primary: '#10B981',
+            secondary: '#FFFFFF',
+        },
+    },
+    error: {
+        duration: 4000,
+        iconTheme: {
+            primary: '#EF4444',
+            secondary: '#FFFFFF',
+        },
+    },
+};
+
+const AppToaster = () => {
+    return (
+        <Toaster toastOptions={toastOptions} />
+    );
+};
+
+export default AppToaster;
